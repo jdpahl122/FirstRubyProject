93d0e00ad1c2057f1789e01333b59dcbbfdcffbc
Jonathan Pahl <jdpahl122@gmail.com>
2014-10-29 19:49:14 -0400

    Added functionality to save an article to DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,25 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article
 	end
+	
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	def index
+		@articles = Article.all
+	end
+
+
+	private 
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
+
+
 end
