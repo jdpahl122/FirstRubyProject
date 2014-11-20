d11f1e499f3155956c063143f3270b1b0a6213a6
Jonathan Pahl <jdpahl122@gmail.com>
2014-11-20 00:14:53 -0500

    Added update capability'

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,30 @@
 class ArticlesController < ApplicationController
 
 	def new
+	  @article = Article.new
 	end
 
 	def create
 		@article = Article.new(article_params)
 
-		@article.save
+		if @article.save
 		redirect_to @article
+		else
+		  render 'new'
+		end
 	end
-	
+	def edit
+	  @article = Article.find(params[:id])
+	end
+	def update
+	  @article = Article.find(params[:id])
+	  
+	  if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
 	def show
 		@article = Article.find(params[:id])
 	end
