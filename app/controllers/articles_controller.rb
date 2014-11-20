e35479240cf7a6a9326934f64fc08eddf69af058
Jonathan Pahl <jdpahl122@gmail.com>
2014-11-20 00:51:26 -0500

    Added delete-- Bug fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,13 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+	
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
+  end
 
 	def index
 		@articles = Article.all
